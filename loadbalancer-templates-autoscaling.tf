196719866f1bf7f201e18daab03aa138a47b9f36
Prasadaws1 <prasadyadav5906@gmail.com>
2024-11-06 13:00:45 +0530

    Update loadbalancer-templates-autoscaling.tf

--- loadbalancer-templates-autoscaling.tf
+++ loadbalancer-templates-autoscaling.tf
@@ -1,3 +1,5 @@
+############################# template creation 1step#######################
+
 resource "aws_launch_configuration" "mylc" {
 name = "terraform-template"
 description = "my new templates"
@@ -6,7 +8,7 @@ image_id = "ami-04a37924ffe27da53"
 instance_type = "t2.micro"
 key_name = "5newpair"
 security_groups = [aws_security_group.mysg.id]
-
+###########script##################
  user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
@@ -19,16 +21,28 @@ security_groups = [aws_security_group.mysg.id]
  EOF
 }
 
-
-resource "aws-elb" "myelb" {
-name = "terraform-ld"
+#######################creating loadbalancer############################
+resource "aws_elb" "myelb" {
+name = "terraformelb"
 subnets = [aws_subnet.two.id, aws_subnet.three.id]
-security_group = [aws_security-group.mysg.id]
+security_groups = [aws_security_group.mysg.id]
 listener {
 instance_port = 80
-instance_protocol = "httpd"
+instance_protocol = "http"
 lb_port = 80
-lb_protocol = "httpd"
+lb_protocol = "http"
 }
 }
 
+#######################autoscalinggroup#################
+resource "aws_autoscaling_group" "myasg" {
+name = "myscale"
+launch_configuration = aws_launch_template.mylc.id
+min_size = 2
+max_size = 5
+desired_capacity = 2
+health_check_type = "EC2"
+
+load_balancers = [aws_elb.myelb.id]
+vpc_zone_identifier = [aws_subnet.two.id, aws_subnet.three.id]
+}
